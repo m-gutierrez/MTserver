45126235bd2d57892852a7963004cf4c5bd42cdb
Michael Gutierrez <M_G@mit.edu>
2014-08-11 22:25:49 -0400

    Initial push for MTServer and MTWorker

---
+++ MTWorker.py
@@ -0,0 +1,167 @@
+'''
+    Written by: Michael Turek
+    2014-04-01
+    Updated: MG 2014-07-20
+    
+    Description: 
+    This script is designed to interface between a device
+    communication class and a server script. It's goal is to make device 
+    side communication single threaded while allowing multiple users to 
+    simultaneously pass commands to it in a conflict free manner. This is 
+    handled in two stages, server side and worker side. Here, the worker side
+    maintains single threading by processing tasks from a python queue, pythons
+    queue library is thread safe.
+    
+'''
+import socket
+import select
+import sys
+import threading
+import Queue
+import copy
+import time
+import errno
+
+DEBUG = False
+
+
+
+"""
+The Worker class handles the control of the physical device 
+and is the only component that needs to be adapted on a 
+device to device basis.
+
+
+The methods to be implemented are:
+1) processTask(self,task) which determines how the 
+    device should react to a particular request
+
+2) sendStatusUpdate(self) which determines how the 
+    readout of the equipment should be encoded into 
+    a message for the clients
+
+Any additional methods can be defined as well as any 
+necessary data fields. The access will be thread-safe
+as long as all the methods are only called from the 
+default ones and not directly by the server.
+"""
+class Worker(threading.Thread):
+    def __init__(self, server,workerName):
+        threading.Thread.__init__(self)
+        
+        self.server = server
+        self.workQueue = Queue.Queue(maxsize=0)
+        self.running = 1
+        self.quitting = 0
+
+        ### Device specific setup and data ###
+        import importlib        
+        self.devicecomm = importlib.import_module('%sComm'%(workerName)).Comm()
+        self.availablecommands = dir(self.devicecomm)
+        
+        self.DEFAULT_UPDATE_INTERVAL = 5 # in sec
+
+        ######################################
+
+        self.updater = Updater(self, self.DEFAULT_UPDATE_INTERVAL)
+        self.updater.start()
+        
+    # Main worker loop, blocks until there is a task
+    # waiting in workQueue, then it processes it
+    def run(self):
+        while self.running == 1:
+            task = self.workQueue.get()
+            self.processTask(task)
+            self.workQueue.task_done()
+
+    # Describes how a particular device should react
+    # to incoming messages.
+    #
+    # Each message is assumed to be of the form:
+    # <TYPE> <ARG1> <ARG2> ...
+    def processTask(self,task):
+        taskArray = task.split(" ")
+        taskType = taskArray[0]
+        taskArgs = taskArray[1:]
+        
+        try:
+            if taskType in self.availablecommands or taskType == "":
+                #Check if task is available in comm class
+                #If available, get handle for method and call with passed args
+                getattr(self.devicecomm,taskType)(*taskArgs)
+                self.sendStatusUpdate()
+            elif taskType == 'PUPDATE':
+                print "STATUS " + str(time.time()) + " "+str(self.devicecomm.internal_state)
+            else:
+                print "Task not recognized: (" + task + ")"
+                print "taskType: ("+taskType+")"
+                print "Available commands (%s)"%(str(self.availablecommands))
+        except Exception as e:
+            print 'Failed in processTask: ',e
+
+    # Encodes the UPDATE message to be distributed to the clients
+    # This can be a readout from the device or any other kind of 
+    # notification
+    #
+    # Each message should be of the form:
+    # STATUS <ARG1> <ARG2> ...
+    def sendStatusUpdate(self):
+        ### Define the arguments to send ########
+        
+        #########################################
+
+        message = "STATUS " + str(time.time()) + " "+str(self.devicecomm.internal_state)
+        self.server.broadcastMessage(message)
+
+
+    # Adds a task to the worker's workQueue. Can be called by the 
+    # server or directly from the Worker thread
+    def acceptTask(self, task):
+        self.workQueue.put(task)
+
+
+    def kill(self):
+        self.running = 0
+        self.workQueue.put("STOP") 
+        
+        self.updater.kill() 
+        self.updater.join() 
+
+
+"""
+The Updater class implements a simple timer which
+forces the Worker to send a status update to all
+clients at predetermined points in time
+"""
+class Updater(threading.Thread):
+    def __init__(self, worker, timeInterval):
+        threading.Thread.__init__(self)
+        self.worker = worker
+        self.timeInterval = timeInterval
+
+        self.changeTimeQueue = Queue.Queue(maxsize=0)
+        
+
+        self.running = 1
+
+    # If there is no waiting time interval change request,
+    # send an UPDATE message to the worker and waits for
+    # a period of time set in self.timeInterval (in seconds)
+    def run(self):
+        while self.running == 1:
+            if not self.changeTimeQueue.empty():
+                self.timeInterval = self.changeTimeQueue.get()
+                self.changeTimeQueue.task_done()
+
+            self.worker.acceptTask("UPDATE")
+            time.sleep(self.timeInterval)
+
+    # Places a request to change the time interval in the
+    # changeTimeQueue. This request will be processed at the
+    # beginning of the next update cycle
+    def changeTimeInterval(self, newTimeInterval):
+        self.changeTimeQueue.put(newTimeInterval)
+
+    def kill(self):
+        self.running = 0
+    
---
+++ MTserver.py
@@ -0,0 +1,286 @@
+'''
+    Written by: Michael Turek 2014-04-01
+    Updated: MG 2014-07-20
+    
+    Description: 
+    This script is design to handle many-to-one communication between multiple users
+    and a single piece of hardware. 
+    In this script a server socket is opened and an instance of the worker created. 
+    The server then waits for connections, creates a new thread for each and relays
+    messages to the worker.
+'''
+
+import socket
+import select
+import sys
+import threading
+import Queue
+import copy
+import time
+import errno
+import string
+import argparse
+
+DEBUG = False
+
+"""
+The Server class is the main entry point of the backend, it listens
+for client connections at a particular port and spawns a ClientThread
+for each incoming connection.
+
+It also handles the multithreading for the entire backend such that
+the Worker thread is implicitly thread safe.
+"""
+class Server:
+    def __init__(self, workerName, port):
+        self.debug = DEBUG
+        
+        #Get the IP address of host computer
+        self.hostname = socket.gethostname()
+        self.host = socket.gethostbyname(self.hostname) 
+        self.port = port 
+
+        self.server = None
+        self.threads = []
+        
+        self.worker = Worker(self,string.split(workerName,'Worker')[0])
+        self.workerName = workerName
+
+        # self.f = file("messageLog.txt", "w")
+
+        self.deadThreads = []
+        self.serverLock = threading.Lock()
+
+    def openSocket(self):
+        try:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            
+            self.server.bind((self.host,self.port))
+            self.server.listen(5)
+
+            print "Server accepting connections on %s ip:%s port: %s"%(self.hostname,self.host,str(self.port))
+
+        except socket.error, (value,message):
+            if self.server:
+                self.server.close()
+
+            print "ERROR: Could not open socket: " + message
+            sys.exit(1)
+
+    def getWorker(self):
+        return self.worker
+
+    # TODO: Make sure that nothing can destroy/remove a thread while
+    # a long message is being broadcast, is it OK to block the server
+    # while sending? -> Probably OK, other thread will allocate memory
+    # for the function call and the message won't get lost
+    def broadcastMessage(self, message):
+        # self.serverLock.acquire()
+        #threads = copy.copy(self.threads)
+        
+        # self.f.write(message)
+
+        for thread in self.threads:
+            thread.sendMessage(message)
+        
+        threadNum = len(self.threads)
+
+        # self.serverLock.release()    
+
+        self.debugMsg("Broadcasting a message to all users: NUM = " + str(threadNum))
+
+    def removeClient(self, clientThread):
+        self.serverLock.acquire()
+
+        self.threads.remove(clientThread)
+        self.deadThreads.append(clientThread)
+
+        self.serverLock.release()
+
+        print "Client removed from the list"
+        print "Length of deadThreads: " + str(len(self.deadThreads))
+
+    def run(self):
+        self.openSocket()
+        self.worker.start()
+
+        inputSources = [self.server,sys.stdin]
+        running = 1
+
+        print "Launching console for " + self.workerName + "...\n"
+        print "Enter HELP for a list of commands"
+
+        while running and not self.worker.quitting:
+            sys.stdout.write(self.workerName + "> ")
+            sys.stdout.flush()
+
+            inputready,outputready,exceptready = select.select(inputSources,[],[])
+
+            for s in inputready:
+                if s == self.server:
+                    # client,client_address = self.server.accept()
+
+                    client = self.server.accept()
+
+                    clientThread = ClientThread(client, self)
+                    print "Client connected:" + str(clientThread.address)
+                    clientThread.start()
+
+                    self.serverLock.acquire()
+                    self.threads.append(clientThread)
+                    self.serverLock.release()
+
+
+                    # Process zombie clients:
+                    self.serverLock.acquire()
+
+                    while len(self.deadThreads) > 0:
+                        thread = self.deadThreads[0]
+                        thread.join()
+                        self.deadThreads.remove(thread)
+
+                        print "Zombie removed"
+
+                    self.serverLock.release()
+
+                elif s == sys.stdin:
+                    text = sys.stdin.readline().rstrip().upper()
+
+                    if text == 'HELP':
+                        print "The available commands are: \n\tHELP \n\tSTATUS \n\tDEVICESTATUS \n\tKILL"
+                    elif text == 'STATUS':
+                        print "Server running on ip: %s port: %s"%(self.host,str(self.port))
+                        print "Current number of connected clients: " + str(len(self.threads))
+                    elif text == 'DEVICESTATUS':
+                        print "Current Device state: "
+                        self.worker.acceptTask('PUPDATE')
+                    elif text == 'KILL':
+                        running = 0
+
+                    else:
+                        print "Command not recognized, enter HELP to see the list of available commands."
+
+        # Shut down all sockets
+        print "Shutting down..."
+        self.server.close()
+        print "Server socket closed"
+
+        for thread in self.threads:
+            thread.kill()
+            thread.join()
+
+        for thread in self.deadThreads:
+            thread.join()
+
+        self.worker.kill()
+        self.worker.join()
+
+        # self.f.close()
+
+        print "Everything is dead"
+
+    # If the debug flag is set to True, print msg to stdout
+    def debugMsg(self, msg):
+        if self.debug:
+            print msg
+
+
+"""
+The ClientThread implements a connection with a single client
+
+It listens at the assigned socket for incoming messages
+from the client and distributes server broadcasts to them.
+
+By default, the maximum length of a message can be 1024B.
+"""
+class ClientThread(threading.Thread):
+    def __init__(self,(client,address), server):
+        threading.Thread.__init__(self)
+
+        self.debug = DEBUG
+
+        self.server = server
+        self.client = client
+        self.address = address
+        self.running = 1
+
+    # Listen to messages from the client and forward them
+    # to the worker thread when they arrive
+    def run(self):
+        # Force an initial update        
+        self.server.getWorker().acceptTask("UPDATE")
+
+        while self.running:
+            data = self.client.recv(8192)
+            if data:
+                self.debugMsg("A client sent data: (" + data + ")")
+
+                # Remove new line at the end of the message
+                data = data.rstrip("\n\r")
+
+                # TODO: add a filter for server-side tasks
+                # TODO: handle the DISCONNECT message
+                self.server.getWorker().acceptTask(data)
+
+            else:
+                self.debugMsg("recv returned null: connection interrupted")
+                self.kill()
+    
+    # Send a given message to the client socket    
+    def sendMessage(self, message):
+        try:
+            self.client.send(message)
+
+        except socket.error, e:
+            print "ERROR: Socket invalidated while sending"
+            self.kill()
+
+    def kill(self):
+        if self.running == 1:
+            self.running = 0
+
+            try:
+                self.debugMsg("Killing the client connection")
+                self.client.shutdown(socket.SHUT_RDWR)
+                self.client.close()
+            
+            except socket.error, e:
+                if isinstance(e.args, tuple):
+                    if e[0] == errno.ENOTCONN:
+                        self.debugMsg("Closing the socket failed (Socket already closed)")
+                    else:
+                        self.debugMsg(str(e.args))
+                else:
+                    self.debugMsg("Error while closing the client connection: " + str(e))
+
+            finally:
+                self.server.removeClient(self)
+
+    # If the debug flag is set to True, print msg to stdout
+    def debugMsg(self, msg):
+        if self.debug:
+            print msg
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Universal server black-boxing the communication with individual equipment")
+
+    parser.add_argument("worker", help="specify the Worker module you want to communicate with")
+    parser.add_argument("-p", "--port", default=12345, type=int, help="specify the port at which you want to broadcast")
+    parser.add_argument("-d", "--debug", action="store_true", help="enable debug messages")
+
+    args = vars(parser.parse_args(sys.argv[1:]))
+
+    import importlib
+    Worker = importlib.import_module("MTWorker").Worker
+    DEBUG = args["debug"]
+
+    try:
+        import procname
+        procname.setprocname(args["worker"])
+    except:
+        print "Procname module not found. Using default procname: python"
+
+    server = Server(args["worker"], args["port"])
+    server.run()
